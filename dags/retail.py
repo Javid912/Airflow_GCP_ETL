2e9186e19e98a8724811b3b19dfd18984badc876
Javid912 <56196165+Javid912@users.noreply.github.com>
2024-02-09 14:00:54 +0100

    Committing unstaged changes before filter-branch operation

--- dags/retail.py
+++ dags/retail.py
@@ -1,14 +1,51 @@
-from airflow.decorators import dag,task
-from datetime import datetime  
+# retail.py
+
+from airflow.decorators import dag, task
+from datetime import datetime
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as sql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
 
 @dag(
-    start_date=datetime(2024,1,1),
+    start_date=datetime(2023, 1, 1),
     schedule=None,
     catchup=False,
-    
+    tags=['retail'],
 )
+def retail():
 
-def retile():
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='javadsatoungar_online_retail',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+    creat_retile_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id = 'creat_retial_database',
+        dataset_id = 'retail',
+        gcp_conn_id='gcp'
+    )
 
-
-retile()
+    gcs_to_raw = aql.load_file(
+        task_id='gcs_to_raw',
+        input_file=File(
+            'gs://marclamberti_online_retail/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_invoices',
+            conn_id='gcp',
+            metadata=Metadata(schema='retail')
+        ),
+        use_native_support=False,
+    )
+    
+retail()
